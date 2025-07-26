c41051bdcf62c8d5127183772a344c4565395910
Sherwin <tucajohnsherwin@gmail.com>
2025-07-26 23:15:25 +0800

    fix(footer): correct alignment issue on both desktop and mobile

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      <p className='bg-[#2F3EC5] text-[.75rem] text-white  text-center py-1 '>@ 2025 Reviewly. All rights reserved.</p>
+    <div className='mt-auto'>
+      <p className='w-full bg-[#2F3EC5] text-[.75rem] text-white text-center py-1'>@ 2025 Reviewly. All rights reserved.</p>
     </div>
   )
 }
